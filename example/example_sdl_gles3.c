e45b9f5c4e7d490b6ad964f6c56cee9571b903ca
zeromake <a390720046@gmail.com>
2023-05-09 00:55:48 +0800

    fix: gles

--- example/example_sdl_gles3.c
+++ example/example_sdl_gles3.c
@@ -17,6 +17,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+
+#ifdef _WIN32
+#include <io.h>
+#define access _access
+#else
+#include <unistd.h>
+#endif
+
 #if __ANDROID_API__ >= 24
 #include <GLES3/gl32.h>
 #elif __ANDROID_API__ >= 21
@@ -73,23 +81,15 @@ int main(int argc, char **argv) {
 
     // Try with these GL attributes
     // SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
-    // SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 8);
+    // SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 0);
 
-    SDL_Window *window = SDL_CreateWindow("NanoVG Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1024, 800,
-        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_ALLOW_HIGHDPI);
-
-    if (!window) { // If it fails, try with more conservative options
-        SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 0);
-        SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 0);
-
-        window = SDL_CreateWindow("Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1024, 800,
-            SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_SHOWN | SDL_WINDOW_ALLOW_HIGHDPI);
-
-        if (!window) { // We were not able to create the window
-            printf("ERROR: SDL_CreateWindow failed: %s", SDL_GetError());
-            return EXIT_FAILURE;
-        }
-    }
+    SDL_Window *window = SDL_CreateWindow(
+        "NanoVG Example",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        1024,
+        800,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_RESIZABLE);
 
     SDL_GLContext context = SDL_GL_CreateContext(window);
     SDL_GL_MakeCurrent(window, context);
@@ -128,17 +128,27 @@ int main(int argc, char **argv) {
 #elif defined(__APPLE__)
 	int fontCJK = nvgCreateFont(vg, "cjk", "/System/Library/Fonts/PingFang.ttc");
 #elif defined(ANDROID)
-    // DroidSansFallback.ttf
-    // DroidSansChinese.ttf
-    // NotoSansSC-Regular.otf
+    char* cjks[] = {
+        "/system/fonts/NotoSansSC-Regular.otf",
+        "/system/fonts/NotoSansCJK-Regular.ttc",
+        "/system/fonts/DroidSansFallback.ttf",
+        "/system/fonts/DroidSansChinese.ttf"
+    };
+    int fontCJK = 0;
+    for (int i = 0; i < 3; i++) {
+        char* cjkPath = cjks[i];
+        if (access(cjkPath, 0) != -1) {
+            fontCJK = nvgCreateFont(vg, "cjk", cjkPath);
+            break;
+        }
+    }
     int fontNormal = nvgCreateFont(vg, "sans", "/system/fonts/Roboto-Regular.ttf");
-	int fontCJK = nvgCreateFont(vg, "cjk", "/system/fonts/NotoSansSC-Regular.otf");
     // int fontEmoji = nvgCreateFont(vg, "emoji", "/system/fonts/NotoColorEmoji.ttf");
     // nvgAddFallbackFontId(vg, fontNormal, fontEmoji);
     nvgAddFallbackFontId(vg, fontNormal, fontCJK);
 #endif
     const char* text = "云耀可以称为所有精王武器中最具有传奇色彩的一柄——并不是因为它本身的力量，而是因为那个神话般的持有者。几百年前，雅加西所使用的武器是一柄名为“天驱”的软剑，为女神菲娜所赠。然而，由于莫巴帝的背叛，菲娜在与世界树融合之际惨遭毁灭的命运，追随她的血族们也大都死在使徒的剑下，从此一蹶不振。而随着女神香消玉殆，神剑中的力量也开始缓慢地衰竭。数百年之后的路维丝时代，传说中的皇帝遇到了精灵黎瑟西尔，而当他为了黎瑟不惜同时对抗三千名精灵族高阶战士时，“天驱”终于耗尽了最后一份力量，断为无数碎片。\n"
-"此战终结，皇帝不仅救下了黎瑟西尔，同时也赢得了白石厅矮人的尊重——矮人们决定为他重新铸造一柄武器。铸剑师们激烈的竞争下，最终白石厅两大家族之一的索瑞森家族靠着自身的实力以及少许运气，获得了这一权利。族长达斯特&#183;索瑞森亲自在白石山脉最大的熔炉中以魔法金属奥利哈康铸造出一柄锋利无比的软剑，祈祷士黎瑟西尔则为这柄武器加持了最适合皇帝的魔法——驾御风的能力。\n"
+"此战终结，皇帝不仅救下了黎瑟西尔，同时也赢得了白石厅矮人的尊重——矮人们决定为他重新铸造一柄武器。铸剑师们激烈的竞争下，最终白石厅两大家族之一的索瑞森家族靠着自身的实力以及少许运气，获得了这一权利。族长达斯特·索瑞森亲自在白石山脉最大的熔炉中以魔法金属奥利哈康铸造出一柄锋利无比的软剑，祈祷士黎瑟西尔则为这柄武器加持了最适合皇帝的魔法——驾御风的能力。\n"
 "雅加西进攻的速度极快，薄薄的软剑在这种速度下很容易为空气阻力本身带偏，但云耀却不会。这柄剑就像是永远将自己包裹在真空中一般，无论周围刮起多大的狂风，它也绝对不会偏转一丝一毫，它将永远随着持有者的意志，如闪电般斩下。\n"
 "这柄剑就是后世无人不知的“云耀”，它同时也是黎瑟西尔与雅加西爱情的见证。\n"
 "皇帝最终因炎龙的诅咒而死在大卢尔德著名的热砂战场上，但武器却被传承给了他的手下败将——雷霆剑圣阿尔萨斯。这柄剑于是跟随着它的新主人默默无闻地走过整个卡那多斯，一直到了严寒的诺德森大陆，之后再也没有人见过云耀，直到伊修托利与路维丝的战争拉开序幕之时。\n";
@@ -149,6 +159,10 @@ int main(int argc, char **argv) {
                 quit=1;
                 break;
         }
+        SDL_GetWindowSize(window, &winWidth, &winHeight);
+        fbWidth = winWidth;
+        fbHeight = winHeight;
+        glViewport(0, 0, fbWidth, fbHeight);
 
 		// Update and render
 		glClearColor(0,0,0,0);
@@ -176,19 +190,18 @@ int main(int argc, char **argv) {
 	    NVGtextRow rows[3];
 	    start = text;
 	    end = text + strlen(text);
-
-	    while ((nrows = nvgTextBreakLines(vg, start, end, 1000, rows, 3))) {
+	    while ((nrows = nvgTextBreakLines(vg, start, end, winWidth - 200, rows, 3))) {
             for (int i = 0; i < nrows; i++) {
                 NVGtextRow* row = &rows[i];
                 nvgText(vg, x, y, row->start, row->end);
                 y += lineh;
                 if (y > (winHeight - 100)) {
-                    y = 100;
-                    x += 1000;
+                    goto loop;
                 }
             }
 		    start = rows[nrows-1].next;
         }
+        loop:
         nvgEndFrame(vg);
 
         SDL_GL_SwapWindow(window);
